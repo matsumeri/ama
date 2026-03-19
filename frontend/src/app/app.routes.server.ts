4fcbdc29585224972d6f115dcb24065efc98f7d1
Dennis Araya Bustamante <dennis.araya@gmail.com>
2026-03-19 20:07:59 +0000

    ci: deploy Angular frontend to GitHub Pages

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,8 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { META_API_MODULES } from './core/data/meta-api-catalog';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'api/:slug',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return META_API_MODULES.map((module) => ({ slug: module.slug }));
+    }
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender
   }
 ];
